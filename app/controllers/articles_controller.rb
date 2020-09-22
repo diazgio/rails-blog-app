520e0d8ddde8a1c94ebb88a1ca6ab51c2f342c51
Giordano Dìaz <diazgiordano@gmail.com>
2020-09-22 16:48:49 -0500

    Create edit file and particial file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,17 +8,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(article_params)
     @article.save
     redirect_to article_path(@article)
   end
+
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end  
 end
