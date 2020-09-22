78df65e71d41ecd61fe2b76f7329b553bfac24b9
Abel <abel.lavieri@gmail.com>
2020-09-22 17:09:32 -0400

    Complete article create and article helper

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -9,4 +11,9 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end  
 end
