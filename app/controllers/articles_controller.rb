8d167344cc2baf43c44e1c819cef56a31a39272d
Giordano Dìaz <diazgiordano@gmail.com>
2020-09-22 17:01:38 -0500

    Create flash message and modificate root

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
     @article = Article.new
   end
@@ -16,16 +16,25 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' was created!"
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
+    flash.notice = "Article '#{@article.title}' was deleted!"
     @article.destroy
     redirect_to articles_path
   end
 
   def edit
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
   end  
 end
