47a9ae2b77e37c7f381fd0337c4535793cf6a8c1
Abel <abel.lavieri@gmail.com>
2020-09-23 17:24:15 -0400

    add authors and sessions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   include ArticlesHelper
+  before_action :require_login, except: [:index, :show]
 
   def index
     @articles = Article.all
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   end  
   root to: 'articles#index'
   resources :tags
+
+  resources :authors
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+
 end
